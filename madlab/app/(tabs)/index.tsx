ca941e47e8fc2e343913eb59ada5dc4bb586a8df
Aditya <adi.official.2990@gmail.com>
2025-04-05 11:12:44 +0530

    Supabase Connect

--- madlab/app/(tabs)/index.tsx
+++ madlab/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Thats Crazy</ThemedText>
+        <ThemedText type="title">Thats Insane</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
